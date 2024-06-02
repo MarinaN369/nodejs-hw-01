8ecbe2623e17e545a129f5eab24bb96204a67ae6
MarinaN369 <143936204+MarinaN369@users.noreply.github.com>
2024-06-02 12:34:33 +0300

    update

--- src/scripts/thanos.js
+++ src/scripts/thanos.js
@@ -1,5 +1,20 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const thanos = async () => {};
+export const thanos = async () => {
+
+    try {const data = await fs.readFile(PATH_DB, 'utf-8');
+        let users = JSON.parse(data);
+
+    const delArray = users.filter(user => Math.random() >= 0.5);
+
+    await fs.writeFile(PATH_DB, JSON.stringify(delArray, null, 2), 'utf-8');
+    console.log('delete contacts:');
+
+}
+    catch (err)
+ {console.error('Error reading file:', err);}
+
+};
 
 await thanos();
